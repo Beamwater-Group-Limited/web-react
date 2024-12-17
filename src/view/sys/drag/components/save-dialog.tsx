021df110005adde7a05ac33c9ac9dc280a2d599f
lichang <dotb116393@163.com>
2024-12-17 13:09:35 +0800

    :bug:fix bug

--- src/view/sys/drag/components/save-dialog.tsx
+++ src/view/sys/drag/components/save-dialog.tsx
@@ -2,6 +2,7 @@ import { Button, Form, Input, message, Modal, Space } from 'antd'
 import { useNavigate } from 'react-router-dom'
 import { ItemType } from '../utils'
 import { saveFlowApi } from '@/api'
+import { useState } from 'react'
 
 const SaveDialogComp = (props: {
   isModalOpen: boolean
@@ -9,6 +10,7 @@ const SaveDialogComp = (props: {
   funcList: ItemType[]
 }) => {
   const [form] = Form.useForm()
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
   /** 取消 */
   const closeHandler = () => {
@@ -17,14 +19,24 @@ const SaveDialogComp = (props: {
   }
   /** 保存 */
   const onFinish = (value: { name: string }) => {
+    setLoading(true)
     saveFlowApi({
       all_function: props.funcList,
       flow_name: value.name
-    }).then(() => {
-      closeHandler()
-      message.success('保存成功')
-      navigate('/settings/flow-list')
     })
+      .then(({ info }) => {
+        if (info.status === 200) {
+          closeHandler()
+          message.success('保存成功')
+          navigate('/flow-list')
+        } else {
+          message.error(info.name)
+        }
+        setLoading(false)
+      })
+      .catch(() => {
+        setLoading(false)
+      })
   }
   return (
     <Modal title="保存设置" footer={<></>} open={props.isModalOpen} onCancel={closeHandler}>
@@ -40,10 +52,12 @@ const SaveDialogComp = (props: {
         </Form.Item>
         <Form.Item>
           <Space>
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" loading={loading} htmlType="submit">
               保存
             </Button>
-            <Button onClick={closeHandler}>取消</Button>
+            <Button loading={loading} onClick={closeHandler}>
+              取消
+            </Button>
           </Space>
         </Form.Item>
       </Form>
