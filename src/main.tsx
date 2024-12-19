5a2f03cc314cb06dff2505eddb1e9f793122e6a5
lichang <lichang@cbtai.com>
2024-12-19 10:42:46 +0800

    :new:add rem.ts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import AppRouterComp from '@/router'
 import '@/assets/style/tailwind.css'
+import '@/utils/rem'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ConfigProvider locale={zhCN}>
