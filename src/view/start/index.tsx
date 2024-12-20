1376ca55514c619c1d2f259db307cbc926f6c2c0
lichang <lichang@cbtai.com>
2024-12-20 16:27:00 +0800

    :new:add enter page

--- src/view/start/index.tsx
+++ src/view/start/index.tsx
@@ -1,6 +1,23 @@
+import { Card } from 'antd'
+import StartPageStyle from './index.module.less'
+import { useNavigate } from 'react-router-dom'
 /** 开始页面 */
 const StartPage = () => {
-  return <div></div>
+  const naviagte = useNavigate()
+  return (
+    <div
+      className={`${StartPageStyle['start_page']} flex items-center justify-end pr-[10vw] gap-[5vw]`}
+    >
+      <Card style={{ width: 300 }} onClick={() => naviagte('/wifi-setting', { replace: false })}>
+        <div className="text-xl font-bold mb-4">WIFI配置</div>
+        <div>对开发板的WiFi进行配置</div>
+      </Card>
+      <Card style={{ width: 300 }} onClick={() => naviagte('/demo', { replace: false })}>
+        <div className="text-xl font-bold mb-4">Demo尝试</div>
+        <div>通过Demo体验我们的产品</div>
+      </Card>
+    </div>
+  )
 }
 
 export default StartPage
