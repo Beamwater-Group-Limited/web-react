bfab1ade634527f73398977e605ccd8a43750464
lichang <lichang@cbtai.com>
2024-12-24 13:38:29 +0800

    :new:add flow-handler controller

--- src/view/sys/video-stream/components/monitor-box.tsx
+++ src/view/sys/video-stream/components/monitor-box.tsx
@@ -1,64 +1,78 @@
-import { useEffect, useRef } from 'react'
-import { Button, message } from 'antd'
+import { forwardRef, Ref, useEffect, useImperativeHandle, useRef } from 'react'
+import { message } from 'antd'
 import WebRtcStreamer from '@/utils/webrtcstreamer'
-const MonitorBoxComp = (props: { onCapture: (file: File) => void }) => {
-  const webRtcServer = useRef<any>(null)
-  const webrtcConfig = useRef({
-    url: 'http://192.168.0.70:8000',
-    options: 'rtptransport=tcp&timeout=60',
-    rtsp: 'rtsp://admin:yuanm201109@192.168.0.111:555/Streaming/Channels/1'
-  })
+import { getRandomString } from '@lichang666/utils'
+const MonitorBoxComp = forwardRef(
+  (props: { onCapture: (file: File) => void; className?: string; rtsp: string }, ref: Ref<any>) => {
+    const webRtcServer = useRef<any>(null)
+    const webrtcConfig = useRef({
+      url: 'http://192.168.0.70:8000',
+      options: 'rtptransport=tcp&timeout=60',
+      rtsp: ''
+    })
+    const videoId = useRef(getRandomString(5))
 
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null)
 
-  const videoInit = async () => {
-    let { rtsp, options, url } = webrtcConfig.current
-    webRtcServer.current = new WebRtcStreamer('video', url)
-    webRtcServer.current.connect(rtsp, options)
+    const videoInit = async () => {
+      if (props.rtsp) {
+        webrtcConfig.current.rtsp = props.rtsp
+      }
+      let { rtsp, options, url } = webrtcConfig.current
+      webRtcServer.current = new WebRtcStreamer(videoId.current, url)
+      webRtcServer.current.connect(rtsp, options)
+    }
+
+    useEffect(() => {
+      if (!window.RTCPeerConnection) {
+        message.error('当前浏览器不支持RTC')
+      }
+      videoInit()
+      return () => {
+        webRtcServer.current.disconnect()
+      }
+    }, [])
+
+    const captureHandler = () => {
+      const video = document.getElementById(videoId.current) as HTMLVideoElement
+      const canvas = canvasRef.current
+      if (!video || !canvas) return
+      const context = canvas.getContext('2d')
+      if (context) {
+        // 设置 canvas 的宽高与 video 一致
+        canvas.width = video.videoWidth
+        canvas.height = video.videoHeight
+
+        // 将视频当前帧绘制到 canvas 上
+        context.drawImage(video, 0, 0, canvas.width, canvas.height)
+        // 将 Canvas 内容转为 Blob 对象
+        canvas.toBlob((blob) => {
+          if (blob) {
+            // 将 Blob 转换为 File 对象
+            const file = new File([blob], 'screenshot.png', { type: 'image/png' })
+            props.onCapture(file)
+          }
+        }, 'image/png')
+      }
+    }
+
+    useImperativeHandle(ref, () => ({
+      captureHandler
+    }))
+    return (
+      <div className={`border-[1px] border-solid border-gray-300 p-2 ${props.className}`}>
+        <span className="mb-4">{props.rtsp}</span>
+        <video
+          id={videoId.current}
+          className="w-[40vw] h-[20vw]"
+          style={{ objectFit: 'fill' }}
+          muted
+        ></video>
+        {/* 隐藏的 Canvas 元素 */}
+        <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+      </div>
+    )
   }
-
-  useEffect(() => {
-    if (!window.RTCPeerConnection) {
-      message.error('当前浏览器不支持RTC')
-    }
-    videoInit()
-    return () => {
-      webRtcServer.current.disconnect()
-    }
-  }, [])
-
-  const captureHandler = () => {
-    const video = document.getElementById('video') as HTMLVideoElement
-    const canvas = canvasRef.current
-    if (!video || !canvas) return
-    const context = canvas.getContext('2d')
-    if (context) {
-      // 设置 canvas 的宽高与 video 一致
-      canvas.width = video.videoWidth
-      canvas.height = video.videoHeight
-
-      // 将视频当前帧绘制到 canvas 上
-      context.drawImage(video, 0, 0, canvas.width, canvas.height)
-      // 将 Canvas 内容转为 Blob 对象
-      canvas.toBlob((blob) => {
-        if (blob) {
-          // 将 Blob 转换为 File 对象
-          const file = new File([blob], 'screenshot.png', { type: 'image/png' })
-          props.onCapture(file)
-        }
-      }, 'image/png')
-    }
-  }
-  return (
-    <div className="absolute top-[1vh] left-[1vw]">
-      <video id="video" className="w-[20vw] h-[10vw]" style={{ objectFit: 'fill' }} muted></video>
-      <Button onClick={captureHandler} className="mt-2">
-        截屏
-      </Button>
-      {/* 隐藏的 Canvas 元素 */}
-      <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
-    </div>
-  )
-}
+)
 
 export default MonitorBoxComp
--- src/view/sys/video-stream/index.tsx
+++ src/view/sys/video-stream/index.tsx
@@ -1,14 +1,49 @@
 import MonitorBoxComp from './components/monitor-box'
+import { Popover } from 'antd'
+import RobotComp from '@/components/robot'
+import { useEffect, useState } from 'react'
 
 /** 视频流处理 */
 const VideoStreamPage = () => {
+  const [open, setOpen] = useState(false)
   const handleCapture = (file: File) => {
     console.log(file)
   }
 
+  useEffect(() => {
+    setOpen(true)
+    setTimeout(() => {
+      setOpen(false)
+    }, 5000)
+  }, [])
+
   return (
     <div>
-      <MonitorBoxComp onCapture={handleCapture} />
+      <Popover
+        content={<span>点击我进行流程配置</span>}
+        placement="left"
+        trigger="click"
+        open={open}
+        onOpenChange={(open) => setOpen(open)}
+      >
+        <RobotComp className="lg:top-[10vh] right-[5vw] sm:bottom-[50vh] base:top-0" />
+      </Popover>
+      <div className="flex items-center justify-center gap-4 mt-[20vh]">
+        <div>
+          <div className="mb-4">流处理控件1</div>
+          <MonitorBoxComp
+            rtsp="rtsp://admin:yuanm201109@192.168.0.111:555/Streaming/Channels/1"
+            onCapture={handleCapture}
+          />
+        </div>
+        <div>
+          <div className="mb-4">流处理控件2</div>
+          <MonitorBoxComp
+            rtsp="rtsp://admin:yuanm201109@192.168.0.112:554/cam/realmonitor?channel=1&subtype=0"
+            onCapture={handleCapture}
+          />
+        </div>
+      </div>
     </div>
   )
 }
