c8ce9ae05ef342f48e2c204cba840e6ad0fb0e38
lichang <lichang@cbtai.com>
2024-12-27 10:44:57 +0800

    :wastebasket:delete useless code

--- src/api/run/index.ts
+++ src/api/run/index.ts
@@ -1,25 +1,5 @@
-import { FlowItemType } from '../flow'
 import { HandleApiType } from './types'
 export * from './types'
-export const runApi = async (
-  data: {
-    image_data: string
-  } & FlowItemType
-): Promise<
-  ApiResponseType<{
-    value: string
-    image_input: string
-  }>
-> => {
-  return fetch(`${window.ipconfig.ip + window.ipconfig.pyserver}/v1/run_flow`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }).then((res) => res.json())
-}
-
 /** 处理借口 */
 export const imgHandleApi = async (
   data: HandleApiType
--- src/view/sys/chat/index.tsx
+++ src/view/sys/chat/index.tsx
@@ -2,7 +2,7 @@ import { BubbleListComp, InputBoxComp, FlowSelectorComp } from './components'
 import { Suggestion } from '@ant-design/x'
 import { useRef, useState } from 'react'
 import { BubbleItemType } from './types'
-import { FlowItemType, runApi } from '@/api'
+import { FlowItemType } from '@/api'
 import { Popover, UploadFile } from 'antd'
 import { InputBoxCompRef } from './components/input-box'
 import RobotComp from '@/components/robot'
@@ -29,59 +29,18 @@ const ChatPage = () => {
   const submitHandler = () => {
     if (!inputBoxRef.current) return
     const { inputValue, setInputValue } = inputBoxRef.current
-    if (fileList.length > 0) {
-      setLoading(true)
-      let file = fileList[0]
-      const reader = new FileReader()
-      reader.onload = async (event) => {
-        if (!event.target || !event.target.result) return
-        let src = event.target.result as string
-        const fileBase64 = src.split(',')[1]
-        if (inputValue === '/' || !currentFlow) return
-        setBubbleList((pre) => [
-          ...pre,
-          {
-            time: new Date().getTime().toString(),
-            content: `<img src="${src}" style="width: 200px; height: 200px;" />`,
-            role: 'user'
-          }
-        ])
-        runApi({
-          image_data: fileBase64,
-          ...currentFlow
-        }).then(({ info, data }) => {
-          setLoading(false)
-          if (info.status === 200) {
-            setLoading(false)
-            setInputValue('')
-            setFileList([])
-            inputBoxRef.current && inputBoxRef.current.setOpen(false)
-            setBubbleList((pre) => [
-              ...pre,
-              {
-                time: new Date().getTime().toString(),
-                content: data.value,
-                role: 'ai'
-              }
-            ])
-          }
-        })
-      }
-      reader.readAsDataURL(file as unknown as Blob)
-    } else {
-      if (inputValue === '' || inputValue === '/' || !currentFlow) return
-      setBubbleList((pre) => {
-        return [
-          ...pre,
-          {
-            time: new Date().getTime().toString(),
-            content: inputValue,
-            role: 'user'
-          }
-        ]
-      })
-      setInputValue('')
-    }
+    if (inputValue === '' || inputValue === '/' || !currentFlow) return
+    setBubbleList((pre) => {
+      return [
+        ...pre,
+        {
+          time: new Date().getTime().toString(),
+          content: inputValue,
+          role: 'user'
+        }
+      ]
+    })
+    setInputValue('')
   }
 
   return (
